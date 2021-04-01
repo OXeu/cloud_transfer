5b0913108430f44e324429a2bcd22998547a2d42
ThankRain <36541432+ThankRain@users.noreply.github.com>
2021-04-01 23:48:34 +0800

    Add files via upload

--- download.py
+++ download.py
@@ -51,7 +51,7 @@ class Download():
 				        f.write(chunk)
 				        f.flush()
 				        #############花哨的下载进度部分###############
-				        done = int(50 * temp_size / total_size)
+				        #done = int(50 * temp_size / total_size)
 				        # 调用标准输出刷新命令行，看到\r 回车符了吧
 				        # 相当于把每一行重新刷新
 			print()  # 避免上面\r 回车符，执行完后需要换行了，不然都在一行显示
